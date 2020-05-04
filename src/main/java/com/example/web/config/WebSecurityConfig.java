3afc1df3c58fe98499ee3ddd0785f644c113b672
AntonScript <antonmamakinscript@gmail.com>
2020-05-04 12:11:42 +0400

    NewAut
    автоизация с помощью встроенного функионала спринга

--- src/main/java/com/example/web/config/WebSecurityConfig.java
+++ src/main/java/com/example/web/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.web.config;
 
+import com.example.web.service.UserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,7 +17,7 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    private DataSource dataSource;
+    private UserSevice userSevice;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,10 +40,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .usersByUsernameQuery("select username, password, active from usr where username=?")
-                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        auth.userDetailsService(userSevice)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
     }
 }
---
+++ src/main/java/com/example/web/service/UserSevice.java
@@ -0,0 +1,19 @@
+package com.example.web.service;
+
+import com.example.web.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSevice implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepo.findByUsername(username);
+    }
+}
